fc86dba6bf5992e5937aa26cb25fae5c6b4332c1
Groven Kwoc <GrovenKwoc@users.noreply.github.com>
2024-04-30 22:20:41 +0800

    测试soulers

--- app/page.tsx
+++ app/page.tsx
@@ -10,6 +10,10 @@ export default function Page() {
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
         <AcmeLogo />
       <Link href="/dashboard"><span>前往控制台</span></Link>
+      <Link href="https://baidu.com"><span>前往控制台aaa</span></Link>
+      <Link href="https://bing.com"><span>前往控制台bbb</span></Link>
+      <Link href="https://bilibili.com"><span>Souler救命</span></Link>
+      <Link href="https://google.com"><span>群主救我</span></Link>
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
