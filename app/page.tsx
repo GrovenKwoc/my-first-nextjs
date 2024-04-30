2b319c9f9ed81e0819374f9bb0e3d422234ce90c
Groven Kwoc <GrovenKwoc@users.noreply.github.com>
2024-04-30 22:29:36 +0800

    试试看

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ import Image from 'next/image';
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
-      <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
+      <div className="flex justify-between h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
         <AcmeLogo />
       <Link href="/dashboard"><span>前往控制台</span></Link>
       <Link href="https://baidu.com"><span>前往控制台aaa</span></Link>
