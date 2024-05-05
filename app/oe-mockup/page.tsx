a87f9dfa96344436c134cf07ca55e58825740785
Groven Kwoc <GrovenKwoc@users.noreply.github.com>
2024-05-05 20:58:59 +0800

    test

--- app/oe-mockup/page.tsx
+++ app/oe-mockup/page.tsx
@@ -9,11 +9,12 @@ export default function Page() {
   const [coeffient, setCoeffient] = useState('');
   const [style, setStyle] = useState('');
   return (
-    <div>
-      <h1>欧易</h1>
+    <div className="h-screen flex flex-col justify-center items-center">
+      <h1 className="text-3xl font-bold">欧易计算器</h1>
+      <div className='p-4'>
       <label>
         盘面价格:{current}
-        <input
+        <input className="ring-2 ring-gray-200 rounded-md hover:ring-4 hover:ring-gray-300 ml-4"
           type="text"
           value={current}
           onChange={(e) => {
@@ -21,10 +22,11 @@ export default function Page() {
           }}
         />
       </label>
-      <br/>
+      </div>
+      <div className='p-4'>
       <label>
         最低价格:{low}
-        <input
+        <input className="ring-2 ring-gray-200 rounded-md hover:ring-4 hover:ring-gray-300 ml-4"
           type="text"
           value={low}
           onChange={(e) => {
@@ -32,10 +34,11 @@ export default function Page() {
           }}
         />
       </label>
-      <br/>
+      </div>
+      <div className='p-4'>
       <label>
         最高价格:{high}
-        <input
+        <input className="ring-2 ring-gray-200 rounded-md hover:ring-4 hover:ring-gray-300 ml-4"
           type="text"
           value={high}
           onChange={(e) => {
@@ -43,36 +46,40 @@ export default function Page() {
           }}
         />
       </label>
-      <br/>
+      </div>
+      <div className='p-4'>
       <label>
         间距:{distance}
-        <input
+        <input className="ring-2 ring-gray-200 rounded-md hover:ring-4 hover:ring-gray-300 ml-4"
           type="text"
           value={distance}
           onChange={(e) => setDistance(e.target.value)}
         />
       </label>
-      <br/>
+      </div>
+      <div className='p-4'>
       <label>
         杠杆系数:{coeffient}
         <input
-          type="text"
+          type="text" className="ring-2 ring-gray-200 rounded-md hover:ring-4 hover:ring-gray-300 ml-4"
           value={coeffient}
           onChange={(e) => setCoeffient(e.target.value)}
         />
       </label>
-      <br/>
+      </div>
+      <div className='p-4'>
       <label>
         等比/等差:{style}
-        <select onChange={(e) => setStyle(e.target.value)}>
+        <select onChange={(e) => setStyle(e.target.value)} className="ring-2 ring-gray-200 rounded-md ml-4">
           <option value="等比">等比</option>
           <option value="等差">等差</option>
         </select>
       </label>
-      <br/>
+      </div>
+      <div className='p-4'>
       <span>单手最小保证金：{(parseFloat(current) * parseFloat(coeffient)).toFixed(2)}</span>
-      <br />
-      <button
+      </div>
+      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-md w-24"
         type="button"
         onClick={() => {
           alert('预估头寸强平价:' + current + ',持仓平均价:' + low);
